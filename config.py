739b15078d24abb2a762eafb1d9d86e495e6168c
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-05-20 07:30:03 +0530

    Update BOT_STATS_TEXT format in config.py

--- config.py
+++ config.py
@@ -85,7 +85,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 #Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 #--------------------------------------------
-BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
+BOT_STATS_TEXT = "<b>BOT UPTIME</b>{uptime}\n👥 **Total Users{total_users}\nPyrogram Version:** v{__version__}\n"
 USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!"
 
 #==========================(BUY PREMIUM)====================#
