ce5cc78e0e8015ed59c6417dc608625f32f0f949
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-05-22 20:59:13 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -269,11 +269,10 @@ async def start_command(client: Client, message: Message):
                 last=message.from_user.last_name,
                 username=None if not message.from_user.username else '@' + message.from_user.username,
                 mention=message.from_user.mention,
-                id=message.from_user.id,
-                effect_id=int(random.choice(EFFECT_IDS))
+                id=message.from_user.id
             ),
-            reply_markup=reply_markup
-        ) 
+            reply_markup=reply_markup,
+            effect_id=int(random.choice(EFFECT_IDS))) 
 
         return
 
