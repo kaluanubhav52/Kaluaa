8fbd02837bc52c0072b7c5a982f352ca96c674fd
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-05-23 16:09:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -123,7 +123,8 @@ async def start_command(client: Client, message: Message):
                 return await message.reply(
                     f"✅ <b>𝗧𝗼𝗸𝗲𝗻 𝘃𝗲𝗿𝗶𝗳𝗶𝗲𝗱!</b>\n\nVαʟɪᴅ ғᴏʀ: {get_exp_time(VERIFY_EXPIRE)}\n\n"
                     "Cʟɪᴄᴋ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ғɪʟᴇ 👇",
-                    reply_markup=InlineKeyboardMarkup(btn)
+                    reply_markup=InlineKeyboardMarkup(btn),
+                    protect_content=True
                 )
 
             # 3️⃣ CASE: 🔥 STRICT ENFORCEMENT BLOCK 🔥
@@ -142,7 +143,8 @@ async def start_command(client: Client, message: Message):
                 ]
                 return await message.reply(
                     f"<b>𝗬𝗼𝘂𝗿 𝘁𝗼𝗸𝗲𝗻 𝗵𝗮𝘀 𝗲𝘅𝗽𝗶𝗿𝗲𝗱. 𝗣𝗹𝗲𝗮𝘀𝗲 𝗿𝗲𝗳𝗿𝗲𝘀𝗵 ʏᴏᴜʀ ᴛᴏᴋᴇ𝗻 ᴛᴏ 𝗰𝗼𝗻𝘁𝗶𝗻𝘂𝗲..</b>\n\n<b>Tᴏᴋᴇɴ Tɪᴍᴇᴏᴜᴛ:</b> {get_exp_time(VERIFY_EXPIRE)}",
-                    reply_markup=InlineKeyboardMarkup(btn)
+                    reply_markup=InlineKeyboardMarkup(btn),
+                    protect_content=True
                 )
 
         # 4️⃣ CASE: Jab banda verified ho (File send karo)
