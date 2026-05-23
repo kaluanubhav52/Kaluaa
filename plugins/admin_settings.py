1b906ee88bb915601e39b0c0aef70bb2919c1bd1
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-05-23 07:28:29 +0530

    Update admin_settings.py

--- plugins/admin_settings.py
+++ plugins/admin_settings.py
@@ -1,7 +1,7 @@
 
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from database.database import database 
+from database.database import *
 from bot import Bot
 
 # Sirf Admin access ke liye
