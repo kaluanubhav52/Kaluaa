045b260163d33451ad23a70e731799de0d5a874e
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-08-18 21:15:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,14 +23,14 @@ APP_ID = int(os.environ.get("APP_ID", "")) #Your API ID from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "") #Your API Hash from my.telegram.org
 #--------------------------------------------
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003817564902")) #Your db channel Id
-OWNER = os.environ.get("OWNER", "O_Deleted_Account_0") # Owner username without @
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "")) #Your db channel Id
+OWNER = os.environ.get("OWNER", "Kcxry") # Owner username without @
 OWNER_ID = int(os.environ.get("OWNER_ID", "5898522531")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
 DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluooo")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluovvoo")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "0"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/pratilipifm0900")
@@ -98,7 +98,7 @@ USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘ
 
 #==========================(BUY PREMIUM)====================#
 
-OWNER_TAG = os.environ.get("OWNER_TAG", "O_Deleted_Account_0")
+OWNER_TAG = os.environ.get("OWNER_TAG", "KCXRY")
 UPI_ID = os.environ.get("UPI_ID", "6398324472@fam")
 QR_PIC = os.environ.get("QR_PIC", "https://i.ibb.co/PGbZztgZ/photo-2026-05-04-16-41-38-7636287934861148164.jpg")
 SCREENSHOT_URL = os.environ.get("SCREENSHOT_URL", f"t.me/O_Deleted_Account_0")
